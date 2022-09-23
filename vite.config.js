9d6b5f121ad16a9da32ed2b22d86219d2951c796
smorris <stefan.morris@shustudios.com>
2022-09-23 10:40:24 -0600

    - app configuration

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
+import fs from 'fs'
+import dotenv from 'dotenv'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const env = dotenv.config()
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  env: env.parsed,
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    host: '0.0.0.0',
+		port: process.env.SERVER_PORT,
+    https: {
+      key: fs.readFileSync(path.resolve(process.env.KEY_PATH, 'privkey.pem')),
+      cert: fs.readFileSync(path.resolve(process.env.CERT_PATH, 'fullchain.pem'))
+    }
+	}
 })
